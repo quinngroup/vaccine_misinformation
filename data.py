59f802f37dc5ec3349c0a58581ce8d45b7176a62
Jonathan Waring <jwaring8@gmail.com>
2017-02-10 22:04:22 -0500

    Removed any mention of xgoogle, as it no longer works

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
 import csv
-from xgoogle.search import GoogleSearch, SearchError #google search tool
 
 #List that stores both url and text of vaccine webpages
 webpage_data = {'url': [],
@@ -11,16 +10,4 @@ with open('Vaccine Dataset.csv', 'r') as csvfile:
     for row in reader:
         webpage_data['url'].append(row[1]) #url is in second cell of csv
 
-#Collect urls of unlabeled data using xgoogle tool
-try:
-    vaccine_search = GoogleSearch("vaccine safety") #term to serach
-    vaccine_search.results_per_page = 10 
-    results = vaccine_search.get_results() #iterable object
-    print len(results)
-    for res in results:
-        print res.title.encode("utf8")
-        print res.desc.encode("utf8")
-        print res.url.encode("utf8")
-        print
-except SearchError, e:
-  print "Search failed: %s" % e
+#Collect urls of unlabeled data
